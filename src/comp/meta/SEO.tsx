fc7eabfd1a76a7e3420d53e8ea4eefa7f1483e74
Joshua Hughes <joshua@plutonus.dev>
2024-04-23 13:27:52 +1000

    feat: added SEO component to project
    The commit introduces a new SEO component to the project. This SEO component is responsible for assigning the meta properties for the website, which plays pivotal role in Search Engine Optimization.
    
    Changes have in the index.tsx file to incorporate the newly added SEO component. The SEO component has been added as a wrapper over the existing components, ensuring that SEO properties are added to all the components in the project.
    
    This feature will significantly enhance the visibility of the website on various search engines.

---
+++ src/comp/meta/SEO.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import Head from "next/head";
+
+const SEO: React.FC<SEOProps> = ({ description = "Facilitate real-time text-based communication between air traffic controllers and pilots within vatSys.", keywords = ["vatSys", "ACARS", "plugin", "vatsim", "vatpac"], title, site_name, site_url = "https://vatacars.com/", twitter_handle }) => (
+	<Head>
+		<title>vatACARS {title ? `- ${title}` : ""}</title>
+		<meta name="description" content={description} />
+		<meta name="keywords" content={keywords?.join(", ")} />
+		<meta name="theme-color" content="#AC8DFB" />
+
+		<meta property="og:type" content="website" />
+		<meta property="og:title" content={title} />
+		<meta property="og:description" content={description} />
+		<meta property="og:site_name" content={site_name} />
+		<meta property="og:url" content={site_url} />
+		{/*<meta property="og:image" content="/static/images/logo-light.png" />*/}
+
+		{/*<meta name="twitter:card" content="summary_large_image" />*/}
+		<meta name="twitter:title" content={title} />
+		<meta name="twitter:description" content={description} />
+		<meta name="twitter:site" content={site_url} />
+		{/*<meta name="twitter:creator" content={twitter_handle} />*/}
+		{/*<meta name="twitter:image" content="/static/images/logo-light.gif" />*/}
+	</Head>
+);
+
+export interface SEOProps {
+	description?: string;
+	lang?: string;
+	meta?: any[];
+	keywords?: string[];
+	title?: string;
+	site_name?: string;
+	site_url?: string;
+	twitter_handle?: string;
+}
+
+export default SEO;
