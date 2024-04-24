aecaa05d93685fac3268187cc0ce393490ffa233
Joshua Hughes <joshua@plutonus.dev>
2024-04-24 12:15:35 +1000

    feat: added new images and updated index.tsx file
    This commit adds two new images: dispatch.png and editor.png to the public/img directory. It also includes significant updates to the src/pages/index.tsx file, introducing new components, such as Navbar and Footer, and modifying the structure and content of the main page. We also provide new links to the vatACARS plugin source code and hub.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,17 @@
 import { useEffect } from "react";
+import Link from "next/link";
+import Image from "next/image";
 
+import Navbar from "../comp/ui/Navbar";
+import Footer from "../comp/ui/Footer";
 import SEO from "../comp/meta/SEO";
 
+import { VscGithubInverted } from "react-icons/vsc";
+import { HiOutlineDownload } from "react-icons/hi";
+
 export default function Index() {
+    const hrs = new Date().getHours();
+
     useEffect(() => {
         const elements = document.querySelectorAll('.grid-background > div') as NodeListOf<HTMLElement>;;
 
@@ -25,61 +34,53 @@ export default function Index() {
     return (
         <>
             <SEO />
-            <div className="relative min-h-screen flex items-center justify-center bg-gray-900 overflow-hidden">
-                <div className="grid-background absolute inset-0 p-2 grid grid-cols-12 gap-2 transform -skew-y-12 scale-150">
-                    <div className="col-span-2 bg-gray-800 rounded animate-pulse"></div>
-                    <div className="col-span-5 bg-gray-800 rounded animate-pulse"></div>
-                    <div className="col-span-1 bg-gray-800 rounded animate-pulse"></div>
-                    <div className="col-span-4 bg-gray-800 rounded animate-pulse"></div>
+            <Navbar />
 
-                    <div className="col-span-5 bg-gray-800 rounded animate-pulse"></div>
-                    <div className="col-span-3 bg-gray-800 rounded animate-pulse"></div>
-                    <div className="col-span-2 bg-gray-800 rounded animate-pulse"></div>
-                    <div className="col-span-2 bg-gray-800 rounded animate-pulse"></div>
+            <section className="relative mt-24 text-center flex flex-col space-y-8">
+                <p className="mx-auto -mt-12 max-w-2xl text-lg text-slate-700">Introducing <span className="text-blue-500">vat</span>ACARS.</p>
 
-                    <div className="col-span-4 bg-gray-800 rounded animate-pulse"></div>
-                    <div className="col-span-7 bg-gray-800 rounded animate-pulse"></div>
-                    <div className="col-span-1 bg-gray-800 rounded animate-pulse"></div>
+                <h1 className="mx-auto max-w-4xl font-display font-medium tracking-tight text-slate-900 text-5xl flex flex-row space-x-2 justify-center">
+                    <span className="inline-block">The</span>
+                    <span className="relative whitespace-nowrap text-blue-500 mx-auto">
+                        <svg aria-hidden="true" viewBox="0 0 418 42" className="absolute top-2/3 left-0 h-[0.58em] w-full fill-blue-300/70" preserveAspectRatio="none"><path d="M203.371.916c-26.013-2.078-76.686 1.963-124.73 9.946L67.3 12.749C35.421 18.062 18.2 21.766 6.004 25.934 1.244 27.561.828 27.778.874 28.61c.07 1.214.828 1.121 9.595-1.176 9.072-2.377 17.15-3.92 39.246-7.496C123.565 7.986 157.869 4.492 195.942 5.046c7.461.108 19.25 1.696 19.17 2.582-.107 1.183-7.874 4.31-25.75 10.366-21.992 7.45-35.43 12.534-36.701 13.884-2.173 2.308-.202 4.407 4.442 4.734 2.654.187 3.263.157 15.593-.78 35.401-2.686 57.944-3.488 88.365-3.143 46.327.526 75.721 2.23 130.788 7.584 19.787 1.924 20.814 1.98 24.557 1.332l.066-.011c1.201-.203 1.53-1.825.399-2.335-2.911-1.31-4.893-1.604-22.048-3.261-57.509-5.556-87.871-7.36-132.059-7.842-23.239-.254-33.617-.116-50.627.674-11.629.54-42.371 2.494-46.696 2.967-2.359.259 8.133-3.625 26.504-9.81 23.239-7.825 27.934-10.149 28.304-14.005.417-4.348-3.529-6-16.878-7.066Z"></path></svg>
+                        <span className="relative">Next Generation</span>
+                    </span>
+                    <span className="inline-block">CPDLC Interface</span>
+                </h1>
 
-                    <div className="col-span-4 bg-gray-800 rounded animate-pulse"></div>
-                    <div className="col-span-2 bg-gray-800 rounded animate-pulse"></div>
-                    <div className="col-span-2 bg-gray-800 rounded animate-pulse"></div>
-                    <div className="col-span-2 bg-gray-800 rounded animate-pulse"></div>
-                    <div className="col-span-2 bg-gray-800 rounded animate-pulse"></div>
+                <span className="text-xl">This project is under development and not yet ready for public use. We'll announce the launch date soon!</span>
 
-                    <div className="col-span-2 bg-gray-800 rounded animate-pulse"></div>
-                    <div className="col-span-5 bg-gray-800 rounded animate-pulse"></div>
-                    <div className="col-span-1 bg-gray-800 rounded animate-pulse"></div>
-                    <div className="col-span-4 bg-gray-800 rounded animate-pulse"></div>
-
-                    <div className="col-span-4 bg-gray-800 rounded animate-pulse"></div>
-                    <div className="col-span-7 bg-gray-800 rounded animate-pulse"></div>
-                    <div className="col-span-1 bg-gray-800 rounded animate-pulse"></div>
-
-                    <div className="col-span-5 bg-gray-800 rounded animate-pulse"></div>
-                    <div className="col-span-1 bg-gray-800 rounded animate-pulse"></div>
-                    <div className="col-span-3 bg-gray-800 rounded animate-pulse"></div>
-                    <div className="col-span-3 bg-gray-800 rounded animate-pulse"></div>
-
-                    <div className="col-span-2 bg-gray-800 rounded animate-pulse"></div>
-                    <div className="col-span-5 bg-gray-800 rounded animate-pulse"></div>
-                    <div className="col-span-1 bg-gray-800 rounded animate-pulse"></div>
-                    <div className="col-span-4 bg-gray-800 rounded animate-pulse"></div>
+                <div className="mt-12 flex flex-col justify-center gap-y-5 sm:flex-row sm:gap-y-0 sm:gap-x-6">
+                    <Link href="https://github.com/vatACARS/plugin">
+                        <span className="cursor-pointer flex flex-row items-center space-x-2 rounded-full py-2 px-4 text-slate-50 font-semibold bg-slate-800 hover:bg-slate-600" data-aos="fade-right" data-aos-duration="2000">
+                            <VscGithubInverted />
+                            <span>Source Code</span>
+                        </span>
+                    </Link>
+                    <Link href="">
+                        <span className="cursor-not-allowed flex flex-row items-center space-x-2 rounded-full py-2 px-4 text-slate-800 ring-slate-800 outline-2 font-semibold bg-slate-200" data-aos="fade-left" data-aos-duration="2000">
+                            <HiOutlineDownload />
+                            <span>vatACARS Hub</span>
+                        </span>
+                    </Link>
                 </div>
 
-                <div className="relative">
-                    <h2 className="text-white text-4xl md:text-8xl font-bold flex flex-row items-center">
-                        Coming
-                        <div className="relative text-sm mx-2">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-caret-up-fill text-blue-500" viewBox="0 0 16 16">
-                                <path d="m7.247 4.86-4.796 5.481c-.566.647-.106 1.659.753 1.659h9.592a1 1 0 0 0 .753-1.659l-4.796-5.48a1 1 0 0 0-1.506 0z" />
-                            </svg>
-                            <div className="absolute -top-12 transform -rotate-45 text-blue-500"><p className="font-light text-base text-white bg-blue-500 rounded-md px-2 py-0">super</p></div>
-                        </div>
-                        Soon
-                    </h2>
+                <div className="flex flex-row items-center pb-12 justify-center space-x-4">
+                    <div className="translate-x-24">
+                        <Image src="/img/dispatch.png" layout="intrinsic" width={537} height={354} />
+                    </div>
+                    <div className="-translate-x-24">
+                        <Image src="/img/editor.png" layout="intrinsic" width={590} height={480} />
+                    </div>
                 </div>
-            </div>
+
+                {/*<div className="relative w-full max-w-4xl mx-auto my-12 h-[1/2]">
+                    <img src="/img/dispatch.png" className="absolute inset-0 object-cover z-10" alt="vatACARS Dispatch Window" />
+                    <img src="/img/editor.png" className="absolute inset-0 object-cover z-20 -top-1/2 translate-x-1/2 translate-y-[15%]" alt="vatACARS Editor Window" />
+                </div>*/}
+            </section>
+
+            <Footer />
         </>
     );
 }
