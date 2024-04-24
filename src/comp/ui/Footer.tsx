9c409b2215e1e1a40d93d28ba64b4eb227626496
Joshua Hughes <joshua@plutonus.dev>
2024-04-24 12:15:44 +1000

    feat: Update UI of Footer and Navbar components
    This commit includes changes to the UI of Footer and Navbar components. The background color of the Navbar and Footer has been changed from gray-50 to slate-100. A disclaimer and affiliation statement has been added to the Footer. A "Source Code" link has been added Navbar.

--- src/comp/ui/Footer.tsx
+++ src/comp/ui/Footer.tsx
@@ -1,11 +1,15 @@
 export default function Navbar() {
     return (
-        <nav className="bg-gray-50 py-24">
+        <nav className="bg-slate-100 py-24">
             <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 text-center">
                 <p className="font-semibold text-xl mb-2">
                     <span className="text-blue-500">vat</span>ACARS
                 </p>
                 <p className="text-sm text-slate-600">© 2024 vatACARS Team. All rights reserved.</p>
+                <div className="mt-2">
+                <p className="text-sm text-slate-400"><span className="font-semibold text-red-300">Disclaimer:</span> The information contained on this website is for informational purposes only and should not be used for air navigation.</p>
+                <p className="text-sm text-slate-400">vatACARS is not affiliated with or endorsed by vatSys nor VATSIM (including any divisions).</p>
+                </div>
             </div>
         </nav>
     );
--- src/comp/ui/Navbar.tsx
+++ src/comp/ui/Navbar.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Navbar() {
     return (
-        <nav className="bg-gray-50">
+        <nav className="bg-slate-100">
             <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
                 <div className="px-12 relative flex h-16 items-center justify-between border-b border-gray-200">
                     <div className="flex items-center">
@@ -16,6 +16,7 @@ export default function Navbar() {
                     </div>
                     <div>
                         <div className="flex space-x-8">
+                            <Link href="https://github.com/vatACARS/plugin"><span className="hover:text-slate-700 px-3 py-2 rounded-md text-sm font-medium text-slate-900 cursor-pointer">Source Code</span></Link>
                             <Link href="/download"><span className="hover:text-slate-700 px-3 py-2 rounded-md text-sm font-medium text-slate-900 cursor-pointer">Download</span></Link>
                             <Link href="/privacy"><span className="hover:text-slate-700 px-3 py-2 rounded-md text-sm font-medium text-slate-900 cursor-pointer">Privacy</span></Link>
                         </div>
