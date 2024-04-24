302a9a87f019cbb2a3e9850557c152af5f1338e3
Joshua Hughes <joshua@plutonus.dev>
2024-04-24 12:45:00 +1000

    feat: add favicon and update logo images in SEO component
    Added a favicon to the site and updated the logo images in the SEO component. This provides a better user experience and improves the site's visibility engines.

--- src/comp/meta/SEO.tsx
+++ src/comp/meta/SEO.tsx
@@ -4,6 +4,7 @@ import Head from "next/head";
 const SEO: React.FC<SEOProps> = ({ description = "Facilitate real-time text-based communication between air traffic controllers and pilots within vatSys.", keywords = ["vatSys", "ACARS", "plugin", "vatsim", "vatpac"], title, site_name, site_url = "https://vatacars.com/", twitter_handle }) => (
 	<Head>
 		<title>vatACARS {title ? `- ${title}` : ""}</title>
+		<link rel="icon" href="/favicon.ico" />
 		<meta name="description" content={description} />
 		<meta name="keywords" content={keywords?.join(", ")} />
 		<meta name="theme-color" content="#AC8DFB" />
@@ -13,14 +14,14 @@ const SEO: React.FC<SEOProps> = ({ description = "Facilitate real-time text-base
 		<meta property="og:description" content={description} />
 		<meta property="og:site_name" content={site_name} />
 		<meta property="og:url" content={site_url} />
-		{/*<meta property="og:image" content="/static/images/logo-light.png" />*/}
+		<meta property="og:image" content="/img/logo.png" />
 
 		{/*<meta name="twitter:card" content="summary_large_image" />*/}
 		<meta name="twitter:title" content={title} />
 		<meta name="twitter:description" content={description} />
 		<meta name="twitter:site" content={site_url} />
 		{/*<meta name="twitter:creator" content={twitter_handle} />*/}
-		{/*<meta name="twitter:image" content="/static/images/logo-light.gif" />*/}
+		<meta name="twitter:image" content="/img/logo.png" />
 	</Head>
 );
 
