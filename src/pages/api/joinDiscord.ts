a8b3318c544247fa6b05cdb4802da615e6b22bc2
Joshua Hughes <joshua@plutonus.dev>
2024-06-04 01:40:58 +1000

    feat: add console log to joinDiscord API endpoint

--- src/pages/api/joinDiscord.ts
+++ src/pages/api/joinDiscord.ts
@@ -30,6 +30,8 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
         body
     });
 
+    console.log(resp.json());
+
     if(resp.status == 204) return res.status(204).json({ success: true, message: "You're already in the server." });
     res.status(200).json({ success: true, message: "You've been added!" });
 }
