8bda936ce5ea05b19aa33c481ddf8e7a3883d524
Joshua Hughes <joshua@plutonus.dev>
2024-06-11 14:40:16 +1000

    feat: added agenda library for task scheduling
    This commit includes the addition of the "Agenda" library for task scheduling. The "invalidate auth token" job is defined to delete a unique auth token from the database. This will help in maintaining the security and validity of auth tokens.

---
+++ src/lib/agenda.ts
@@ -0,0 +1,21 @@
+const Agenda = require("agenda");
+import { PrismaClient } from "@prisma/client";
+
+const agenda = new Agenda({db: {address: process.env.database_url, collection: "tasks" }});
+const prisma = new PrismaClient();
+
+agenda.define("invalidate auth token", async job => {
+    const { token } = job.attrs.data;
+
+    const dbToken = await prisma.authToken.findUnique({
+        where: { token }
+    });
+
+    if(dbToken) await prisma.authToken.delete({
+        where: { token }
+    });
+});
+
+agenda.start();
+
+export default agenda;
