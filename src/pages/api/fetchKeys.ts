43d0a86e0cb98d3b86de04b3a3a21d4789382635
Joshua Hughes <joshua@plutonus.dev>
2024-06-11 12:53:18 +1000

    feat: Add fetchKeys and generateKey API routes
    This commit includes the implementation of two new API routes: fetchKeys.ts and generateKey.ts. The former is responsible for fetching the user's API keys, while the latter is used to generate a new API key for the user. The prisma client is used for database interactions in both routes.

---
+++ src/pages/api/fetchKeys.ts
@@ -0,0 +1,15 @@
+import { getIronSession } from "iron-session";
+import { sessionOptions, SessionData } from "../../lib/session";
+import { NextApiRequest, NextApiResponse } from "next";
+import type { AuthTokenLink } from "../../lib/types";
+
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export default async function userRoute(req: NextApiRequest, res: NextApiResponse<AuthTokenLink | any>) { // TODO: "any" is ambiguous, we should have an "ApiResponse" type
+	const session = await getIronSession<SessionData>(req, res, sessionOptions);
+    if (!session.user) return res.status(401).json({ success: false, message: "Not logged in" });
+
+    return res.json({ success: true, apiKeys: session.user.data.authToken });
+}
---
+++ src/pages/api/generateKey.ts
@@ -0,0 +1,41 @@
+import { randomBytes } from "node:crypto";
+import { getIronSession } from "iron-session";
+import { sessionOptions, SessionData } from "../../lib/session";
+import { NextApiRequest, NextApiResponse } from "next";
+import type { AuthTokenLink } from "../../lib/types";
+
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export default async function userRoute(req: NextApiRequest, res: NextApiResponse<AuthTokenLink | any>) { // TODO: "any" is ambiguous, we should have an "ApiResponse" type
+	const session = await getIronSession<SessionData>(req, res, sessionOptions);
+    if (!session.user) return res.status(401).json({ success: false, message: "Not logged in" });
+
+    let VatACARSUser = await prisma.vatACARSUser.findUnique({
+        where: { cid: session.user.data.cid },
+        include: { auth_token: true }
+    });
+
+    if(VatACARSUser.auth_token) return res.status(401).json({ success: false, message: "You have reached your limit of (1) API key." });
+
+    const token = `vAcV1-${randomBytes(26).toString('hex').slice(0, 26)}`;
+    const created = new Date();
+    const expires = new Date(new Date().setMonth(new Date().getMonth() + 1))
+    await prisma.vatACARSUser.update({
+        where: { cid: session.user.data.cid },
+        data: {
+            auth_token: {
+                create: {
+                    token,
+                    created,
+                    expires
+                }
+            }
+        }
+    });
+
+    session.user.data.authToken = [{ token, created, expires }];
+
+    return res.status(200).json({ success: true, token, created, expires, message: `Successfully created an API key for ${session.user.data.name_first}.` });
+}
