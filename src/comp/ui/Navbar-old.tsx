fb22d242959ca679c0082871ebb9320e5a34064f
Joshua Hughes <joshua@plutonus.dev>
2024-07-29 04:49:04 +1000

    refactor: Simplify Navbar component structure
    Simplified the Navbar component by removing unnecessary div wrappers and restructuring the layout for better readability. Also, replaced double quotes with single quotes for consistency.

---
+++ src/comp/ui/Navbar-old.tsx
@@ -0,0 +1,44 @@
+import Link from "next/link";
+import Image from "next/image";
+
+import useUser from "../../lib/useUser";
+
+export default function Navbar() {
+    const { user } = useUser();
+
+    return (
+        <nav className="bg-slate-100">
+            <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+                <div className="px-12 relative flex h-16 items-center justify-between border-b border-gray-200">
+                    <div className="relative w-32 flex items-center">
+                        <div className="flex-shrink-0">
+                            <div className="font-semibold text-lg h-8 cursor-pointer">
+                                <Link href="/">
+                                    <Image src="/img/vatacars-logo.png" layout="fill" objectFit="contain" />
+                                </Link>
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        <div className="flex space-x-8">
+                            <Link href="https://github.com/vatACARS/plugin"><span className="hover:text-slate-700 px-3 py-2 rounded-md text-sm font-medium text-slate-900 cursor-pointer">Source Code</span></Link>
+                            {user && user?.data.authorised && (<>
+                                <Link href="/me">
+                                    <span className="hover:text-slate-700 px-3 py-2 rounded-md text-sm font-medium text-slate-900 cursor-pointer">{user.data.name_first}</span>
+                                </Link>
+                                <Link href="/api/logout">
+                                    <span className="hover:text-slate-700 px-3 py-2 rounded-md text-sm font-medium text-slate-900 cursor-pointer">Sign out</span>
+                                </Link>
+                            </>)}
+                            {user && !user?.data.authorised && (
+                                <Link href="/api/oauth">
+                                    <span className="hover:text-slate-700 px-3 py-2 rounded-md text-sm font-medium text-slate-900 cursor-pointer">Sign in</span>
+                                </Link>
+                            )}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    );
+}
--- src/comp/ui/Navbar.tsx
+++ src/comp/ui/Navbar.tsx
@@ -1,44 +1,29 @@
-import Link from "next/link";
-import Image from "next/image";
-
-import useUser from "../../lib/useUser";
+import Link from 'next/link';
+import Image from 'next/image';
 
 export default function Navbar() {
-    const { user } = useUser();
-
     return (
-        <nav className="bg-slate-100">
-            <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-                <div className="px-12 relative flex h-16 items-center justify-between border-b border-gray-200">
-                    <div className="relative w-32 flex items-center">
-                        <div className="flex-shrink-0">
-                            <div className="font-semibold text-lg h-8 cursor-pointer">
-                                <Link href="/">
-                                    <Image src="/img/vatacars-logo.png" layout="fill" objectFit="contain" />
-                                </Link>
-                            </div>
-                        </div>
-                    </div>
-                    <div>
-                        <div className="flex space-x-8">
-                            <Link href="https://github.com/vatACARS/plugin"><span className="hover:text-slate-700 px-3 py-2 rounded-md text-sm font-medium text-slate-900 cursor-pointer">Source Code</span></Link>
-                            {user && user?.data.authorised && (<>
-                                <Link href="/me">
-                                    <span className="hover:text-slate-700 px-3 py-2 rounded-md text-sm font-medium text-slate-900 cursor-pointer">{user.data.name_first}</span>
-                                </Link>
-                                <Link href="/api/logout">
-                                    <span className="hover:text-slate-700 px-3 py-2 rounded-md text-sm font-medium text-slate-900 cursor-pointer">Sign out</span>
-                                </Link>
-                            </>)}
-                            {user && !user?.data.authorised && (
-                                <Link href="/api/oauth">
-                                    <span className="hover:text-slate-700 px-3 py-2 rounded-md text-sm font-medium text-slate-900 cursor-pointer">Sign in</span>
-                                </Link>
-                            )}
-                        </div>
+        <section className="w-full px-8 text-slate-200 bg-dark z-50">
+            <div className="container flex flex-col flex-wrap md:flex-row items-center justify-between py-5 mx-auto max-w-7xl">
+                <div className="relative flex flex-row">
+                    <div className="flex items-center mb-5 font-medium lg:items-center lg:justify-center md:mb-0 w-32 h-8">
+                        <Link href="/">
+                            <Image src="/img/vatacars-logo-dark.png" layout="fill" objectFit="contain" />
+                        </Link>
                     </div>
                 </div>
+                <nav className="flex flex-wrap items-center mb-5 text-base md:mb-0 md:pl-8 md:ml-8 md:border-l md:border-notsodark">
+                    <Link href="/">
+                        <span className="mr-6 font-medium leading-6 text-notsodark hover:text-slate-50 transition-all duration-300 cursor-pointer">Home</span>
+                    </Link>
+                    <Link href="/">
+                        <span className="mr-6 font-medium leading-6 text-notsodark hover:text-slate-50 transition-all duration-300 cursor-pointer">Source Code</span>
+                    </Link>
+                    <Link href="/">
+                        <span className="mr-6 font-medium leading-6 text-notsodark hover:text-slate-50 transition-all duration-300 cursor-pointer">Sign In</span>
+                    </Link>
+                </nav>
             </div>
-        </nav>
+        </section>
     );
 }
