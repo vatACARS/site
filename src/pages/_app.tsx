47a804119c3b83169e46567a6b599e79672adcce
Joshua Hughes <joshua@plutonus.dev>
2024-12-08 17:06:52 +0100

    Init

---
+++ src/pages/_app.tsx
@@ -0,0 +1,5 @@
+export default ({ Component, pageProps: { ...pageProps } }) => {
+    return (
+        <Component {...pageProps} />
+    )
+}
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    content: [
+        "./src/**/*.{js,jsx,ts,tsx}"
+    ],
+    darkMode: "class",
+    theme: {
+        extend: {
+        }
+    },
+    variants: {
+        extend: {}
+    },
+    plugins: []
+}
