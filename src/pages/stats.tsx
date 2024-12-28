718c2f4c7d930d16116fe71dd86d8fcd5355168e
Joshua Hughes <joshua@plutonus.dev>
2024-12-28 16:02:57 +0100

    feat: implement statistics display with data fetching from API

--- src/pages/stats.tsx
+++ src/pages/stats.tsx
@@ -1,3 +1,22 @@
+import { useEffect, useState } from "react";
+
 export default () => {
-    return <p>Stats</p>
+    const [statistics, setStatistics] = useState({ stations: [], messages: [] });
+
+    useEffect(() => {
+        fetch('/api/statistics')
+            .then(res => res.json())
+            .then(data => setStatistics(data));
+    }, []);
+    
+    return (
+        <div>
+            <h1>Statistics</h1>
+            <ul>
+                <li>Stations: {statistics.stations.length}</li>
+                <li>Messages: {statistics.messages.length}</li>
+            </ul>
+            <pre>{JSON.stringify(statistics, null, 2)}</pre>
+        </div>
+    )
 }
