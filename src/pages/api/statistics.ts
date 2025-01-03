c1b4c025ba93422b473356ec90318ca4f0053b73
Joshua Hughes <joshua@plutonus.dev>
2025-01-03 21:23:29 +0100

    feat: update API endpoint to fetch connected data and include in response

--- src/pages/api/statistics.ts
+++ src/pages/api/statistics.ts
@@ -3,10 +3,11 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import { withRatelimitMiddleware } from '@lib/ratelimitMiddleware';
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-    const stations = await fetch('http://localhost:3000/data/stations').then(res => res.json());
-    const messages = await fetch('http://localhost:3000/data/messages').then(res => res.json());
+    const connected = await fetch('https://api.vatacars.com/data/connected').then(res => res.json());
+    const stations = await fetch('https://api.vatacars.com/data/stations').then(res => res.json());
+    const messages = await fetch('https://api.vatacars.com/data/messages').then(res => res.json());
 
-    return res.status(200).json({ stations, messages });
+    return res.status(200).json({ connected, stations, messages });
 }
 
 export default withRatelimitMiddleware(handler);
